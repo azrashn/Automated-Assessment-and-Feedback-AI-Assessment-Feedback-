acd10d1ed33628b359b84110f743782853238c7c
Anday Turgut <at.andayturgut@gmail.com>
2026-01-11 21:45:42 +0300

    Create QuestionRepository class with basic methods

---
+++ src/repositories/question_repo.py
@@ -0,0 +1,21 @@
+class QuestionRepository: 
+
+    def __init__(self, db): 
+
+        self.db = db 
+
+    def add_question(self, q): 
+
+        pass 
+
+    def get_by_id(self, qid): 
+
+        return None 
+
+    def find_questions(self, difficulty, skill_category): 
+
+        return [] 
+
+    def get_all(self): 
+
+        return []

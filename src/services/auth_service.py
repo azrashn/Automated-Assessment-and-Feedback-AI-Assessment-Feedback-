194f9ef14b31f8461cd2cad97f33982888c2df2f
CgtyShn10 <cagitayss@gmail.com>
2026-01-11 20:12:26 +0300

    Update auth_service.py

---
+++ src/services/auth_service.py
@@ -0,0 +1,10 @@
+class AccountService:
+    def __init__(self, db):
+        self.db = db
+        # context vs. sonra eklenecek
+
+    def create_account(self, data):
+        pass
+
+    def login(self, data):
+        pass

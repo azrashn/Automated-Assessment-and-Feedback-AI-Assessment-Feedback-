f772eb98f45608c240b61bbe00f310b7c1c47180
CgtyShn10 <cagitayss@gmail.com>
2026-01-13 22:08:12 +0300

    Update auth_routes.py

--- src/api/auth_routes.py
+++ src/api/auth_routes.py
@@ -1,13 +1,39 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from src.database import get_db
+from src.services.auth_service import AccountService
+from src.schemas.auth import UserCreate, UserLogin
 
 router = APIRouter()
 
 @router.post("/register")
-def register(db: Session = Depends(get_db)):
-    return {"msg": "Register Endpoint Hazır"}
+def register(user: UserCreate, db: Session = Depends(get_db)):
+    """
+    FR-1: Öğrenci kaydı oluşturur.
+    """
+    service = AccountService(db)
+    new_student = service.create_account(user)
+    
+    # Frontend'in hata almaması için basit JSON dönüyoruz
+    return {
+        "message": "Kayıt Başarılı",
+        "user_id": new_student.user_id,
+        "email": new_student.email
+    }
 
 @router.post("/login")
-def login(db: Session = Depends(get_db)):
-    return {"msg": "Login Endpoint Hazır"}
+def login(user: UserLogin, db: Session = Depends(get_db)):
+    """
+    FR-1: Giriş yapar ve yönlendirme bilgisini döner.
+    """
+    service = AccountService(db)
+    u = service.login(user)
+    
+    # Frontend'in beklediği yanıt formatı
+    return {
+        "user_id": u.user_id,
+        "username": u.username,
+        "role": u.role,
+        "redirect": "dashboard.html",
+        "msg": "Giriş Başarılı"
+    }

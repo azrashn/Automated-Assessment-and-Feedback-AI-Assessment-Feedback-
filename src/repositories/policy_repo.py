1e3bf6b1d68084b1a527e9c071f0cbc5c01b9799
CgtyShn10 <cagitayss@gmail.com>
2026-01-13 22:02:28 +0300

    Update policy_repo.py

--- src/repositories/policy_repo.py
+++ src/repositories/policy_repo.py
@@ -1,10 +1,14 @@
+from sqlalchemy.orm import Session
+from src.models.security import SecurityPolicy, SecurityLog
+
 class PolicyRepository: 
 
- def __init__(self, db): 
+ def __init__(self, db: Session):
   self.db = db 
 
  def query_rules(self): 
-  return []
+  return self.db.query(SecurityPolicy).filter(SecurityPolicy.is_active == True).all()
 
- def insert_log_record(self, log): 
-  pass 
+ def insert_log_record(self, log: SecurityLog): 
+  self.db.add(log)
+  self.db.commit()
